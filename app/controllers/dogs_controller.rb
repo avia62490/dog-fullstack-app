4709b6ed301c599d1239bc5c8340602a24878f67
Alex Via <alexandervia@gmail.com>
2022-09-08 11:27:04 -0500

    creates Dog model

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,7 @@
 class DogsController < ApplicationController
 
   def index
+    @name = 'Fido'
     render 'dogs/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'dogs/index'
+  
 end
